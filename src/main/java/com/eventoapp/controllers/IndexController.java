342f23ed5ba29301bbf08912dd66dad084e8c5a9
Ivo Henrique <ivo.henrique.oliveira@outlook.com.br>
2020-10-20 16:58:55 -0300

    Elaboração Banco de dados, interação entre o front e o banco.

--- src/main/java/com/eventoapp/controllers/IndexController.java
+++ src/main/java/com/eventoapp/controllers/IndexController.java
@@ -1,12 +1,13 @@
-package com.eventoapp.eventoapp.controllers;
+package com.eventoapp.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class IndexController {
+
     @RequestMapping("/")
-   public String index(){
-       return "index";
-   }
+    public String index() {
+        return "index";
+    }
 }
