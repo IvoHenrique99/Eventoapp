15a7901fc317cc1725098858ec701e3d5df398e5
Alexandre Santos <alexandresantos@Alexandres-MacBook-Air.local>
2020-11-06 12:21:59 -0300

    Added java validation

--- src/test/java/com/eventoapp/EventoappApplicationTests.java
+++ src/test/java/com/eventoapp/EventoappApplicationTests.java
@@ -1,15 +1,16 @@
 package com.eventoapp;
 
 import org.junit.Ignore;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class EventoappApplicationTests {
 
-	@Ignore
 	@Test
-	void contextLoads() {
+	public void contextLoads() {
+		Assertions.assertTrue(true);
 	}
 
 }
